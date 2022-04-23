efbb9644882b7df3b977a4bd3cbf3694f51a50be
Husam_Dev <72893731+Husam-Dev@users.noreply.github.com>
2022-04-24 01:24:24 +0200

    Create kdpremium.lua

---
+++ kdpremium.lua
@@ -0,0 +1,9 @@
+local fuunctioon sendUINotification(thetitle, thetext, time)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = thetitle;
+        Text = thetext;
+        Duration = time;
+    })
+end
+
+sendUINotification('Husam#0001', "kdpremium.lua (altcontrol) disabled temprorary, if you're a user then just let the owner know about this, if you're owner then dm me (Husam#0001) for details and so I can fix", 100)
