28f7933476fc3da68aca31b64e5d67d900f45705
VexXzZz <121412984+VexXzZz@users.noreply.github.com>
2023-01-14 20:11:17 +0100

    Update CosmoComplex.lua

--- CosmoComplex.lua
+++ CosmoComplex.lua
@@ -1,1 +1,1 @@
-game:GetService("Players").LocalPlayer:Kick("Outdated version. Join Disord.gg/Altcontrol for the new or dm\n\n- Husam#0001")
+game:GetService("Players").LocalPlayer:Kick("Outdated version. Join Disord.gg/ for the new or dm\n\n- ")
